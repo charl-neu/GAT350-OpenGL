5d75d5cfedf2779ff9e79afe98f80be6d65f4027
charl-neu <cshenkir@student.neumont.edu>
2025-10-15 09:42:03 -0600

    Shader

---
+++ Source/Engine/Resources/Shader.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Resource.h"
+#include <glad/glad.h>
+
+
+namespace neu
+{
+	class Shader : public Resource {
+	public:
+		~Shader();
+
+		bool Load(const std::string& filename, GLuint shaderType);
+
+	public:
+		GLuint m_shader = 0;
+	};
+}
